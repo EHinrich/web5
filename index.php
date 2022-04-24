2539c9b3b2b8dd5c2fdcec4bbe5ec5b0a5fbec3f
EHinrich <90458838+EHinrich@users.noreply.github.com>
2022-04-24 17:57:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -224,7 +224,7 @@ else {
     $db = new PDO('mysql:host=localhost;dbname=u41181', $user, $password, array(PDO::ATTR_PERSISTENT => true));
 
   try {
-    $stmt = $db->prepare("UPDATE form2 SET name, email, year, sex, number_of_limbs, superpowers, biography, checkbox WHERE login = '".$_SESSION['login']."'");
+    $stmt = $db->prepare("UPDATE form2 SET name, email, year, sex, number_of_limbs, superpowers, biography, checkbox WHERE login = '$_SESSION['login']'");
 
     $name = $_POST['name'];
     $email = $_POST['email'];
